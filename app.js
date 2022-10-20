48b06b867ade05fe1ccbd445c54d400de3b39af7
FlorianDarques <107691809+FlorianDarques@users.noreply.github.com>
2022-10-20 14:51:53 +0200

    à faire
    lock le jeu une fois fini

--- app.js
+++ app.js
@@ -1,70 +1,102 @@
-
-
-// état de la partie (en cours ou fini) 
-// let gameOn = true
-
 // nombre de tour
 let turn = 0
-// joueur 1
-// const playerOne = "X"
-// joueur 2 (pvp || pve par la suite)
-// const playerTwo = "O"
-// joueur actif 
-// let activePlayer
-// console.log(gameStatus[0])
-// en cas de victoire 
-// const winner = () => 'le joueur ' + activePlayer + ' a gagné !'
-// en cas d'égalité 
-// const draw = () => 'Draw !'
-// qui joue & afficher le joueur en cours dans le h2
-// const playerTurn = () => "c'est au tour du joueur " + activePlayer
-
-
-// écrire qui joue
+// les joueurs
+playerOne = "X"
+playerTwo = "O"
+// joueur actuel
 const statut = document.querySelector("h2")
+// à bosser pour vérrouiller le jeu
+let jeu = true
 
 // récupération des class case dans un selectorall et mis en variable puis traduit en tableau avec Array.from
 let gameCase = Array.from(document.querySelectorAll(".case"))
 
-gameCase.forEach(cell => cell.addEventListener("click", () => {
-    turn++
-    if (turn % 2 === 0) {
-        if (cell.innerHTML === "") {
-            cell.innerHTML = "X"
-            checkWin()
+// pour chaque div (nommé cell) dans la boucle forEach de mon gameCase,
+// au click, mettre un X et incrémmenté de un le modulo pour changer de joueur
+// et ainsi de suite jusqu'à ce que checkwin ait ça condition de remplie
+    gameCase.forEach(cell => cell.addEventListener("click", () => {
+        if (turn % 2 === 0) {
+            if (cell.innerHTML === "") {
+                cell.innerHTML = playerOne
+                turn++
+                checkWin()
+            }
         }
-    }
-    else {
-        if (cell.innerHTML === "") {
-            cell.innerHTML = "O"
-            checkWin()
+        else {
+            if (cell.innerHTML === "") {
+                cell.innerHTML = playerTwo
+                turn++
+                checkWin()
+            }
         }
-    }
+    }))
 
-}))
+
 function checkWin() {
     if (gameCase[0].innerHTML !== "" && gameCase[0].innerHTML === gameCase[1].innerHTML && gameCase[1].innerHTML === gameCase[2].innerHTML) {
-        console.log('stop')
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[3].innerHTML !== "" && gameCase[3].innerHTML === gameCase[4].innerHTML && gameCase[4].innerHTML === gameCase[5].innerHTML) {
-        console.log("test")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[6].innerHTML !== "" && gameCase[6].innerHTML === gameCase[7].innerHTML && gameCase[7].innerHTML === gameCase[8].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[0].innerHTML !== "" && gameCase[0].innerHTML === gameCase[4].innerHTML && gameCase[4].innerHTML === gameCase[8].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[2].innerHTML !== "" && gameCase[2].innerHTML === gameCase[4].innerHTML && gameCase[4].innerHTML === gameCase[6].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[0].innerHTML !== "" && gameCase[0].innerHTML === gameCase[3].innerHTML && gameCase[3].innerHTML === gameCase[6].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[1].innerHTML !== "" && gameCase[1].innerHTML === gameCase[4].innerHTML && gameCase[4].innerHTML === gameCase[7].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
     if (gameCase[2].innerHTML !== "" && gameCase[2].innerHTML === gameCase[5].innerHTML && gameCase[5].innerHTML === gameCase[8].innerHTML) {
-        alert("stop")
+        if (turn % 2 === 0) {
+            statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
+        } else {
+            statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
+        }
     }
 }
+
+document.querySelector('#rematch').addEventListener('click', rematch)
+
+function rematch() {
+    gameCase.forEach(cell => cell.innerHTML = "")
+    turn = 0
+    statut.innerHTML = ""
+}
+
+
