58a3d288647f1309481c54aa5f229b0910081ce9
FlorianDarques <107691809+FlorianDarques@users.noreply.github.com>
2022-10-25 10:21:46 +0200

    scoring en cours

--- app2.js
+++ app2.js
@@ -1,12 +1,6 @@
 // nombre de tour
 let turn = 0
 
-//test du scoring
-let score = 0
-let scoreP1 = document.querySelector("#p1")
-let scoreP2 = document.querySelector("#p2")
-
-
 // les joueurs
 const playerOne = "X"
 const playerTwo = "O"
@@ -17,46 +11,55 @@ const statut = document.querySelector("h2")
 
 // vérrouiller le jeu
 let jeu = true
-
 // récupération des class case dans un selectorall et mis en variable puis traduit en tableau avec Array.from
 let gameCase = Array.from(document.querySelectorAll(".case"))
 
-// si l'état du jeu est différent de faux (donc vrai) alors le jeu a lieu
-// pour chaque div (nommé cell) dans la boucle forEach de mon gameCase,
-// au click, mettre un X et incrémmenté de un le modulo pour changer de joueur
-// et ainsi de suite jusqu'à ce que checkwin ait ça condition de remplie
+// à bosser , affichage du score des joueurs
+let scorePlayerOne = document.querySelector('#p1')
+let scorePlayerTwo = document.querySelector('#p2')
+
+// mise en place du bot
 if (jeu !== false) {
     gameCase.forEach(cell => cell.addEventListener("click", () => {
-        if (turn % 2 === 0) {
-            if (cell.innerHTML === "" && jeu !== false) {
-                cell.innerHTML = playerOne
-                turn++
-                statut.innerHTML = "C'est au tour du " + playerTwo
-                checkWin()
-            }
-        }
-        else {
-            if (cell.innerHTML === "" && jeu !== false) {
-                cell.innerHTML = playerTwo
-                turn++
-                statut.innerHTML = "C'est au tour du " + playerOne
+        if (jeu !== false && cell.innerHTML === "") {
+            turn++
+            cell.innerHTML = playerOne
+            checkWin()
+            if (jeu === true) {
+                click()
                 checkWin()
             }
         }
     }))
 }
-
+// boucle qui recommence le temps que "" contient un X ou un O 
+function click() {
+    for (i = 0; i < 100; i++) {
+        const random = randomNb();
+        if (gameCase[random].innerHTML === "X" || gameCase[random].innerHTML === "O" && jeu === true) {
+            continue
+        }
+        else {
+            gameCase[random].innerHTML = computer
+            turn++
+            break
+        }
+    }
+}
+// typical nb aléatoire entier
+function randomNb() {
+    // console.log(Math.floor(Math.random() * 8))
+    return Math.floor(Math.random() * 8)
+}
 // vérifie les cas de victoire, 
 // une fois vérifié et les conditions remplient, affiche le gagnant et passe l'état du jeu en false pour le vérrouiller
 function checkWin() {
     if (gameCase[0].innerHTML !== "" && gameCase[0].innerHTML === gameCase[1].innerHTML && gameCase[1].innerHTML === gameCase[2].innerHTML) {
         if (turn % 2 === 0) {
             statut.innerHTML = 'Le joueur ' + playerTwo + ' à gagné '
-            scoreP2.innerHTML = score + 1
             jeu = false
         } else {
             statut.innerHTML = 'Le joueur ' + playerOne + ' à gagné '
-            scoreP1.innerHTML = score + 1
             jeu = false
         }
     }
@@ -124,16 +127,11 @@ function checkWin() {
         }
     }
 }
-
 // bouton de clear de la partie 
 document.querySelector('#rematch').addEventListener('click', rematch)
 function rematch() {
     gameCase.forEach(cell => cell.innerHTML = "")
     turn = 0
-    scoreP1.innerHTML = score - score 
-    scoreP2.innerHTML = score - score 
     jeu = true
     statut.innerHTML = ""
 }
-
-
